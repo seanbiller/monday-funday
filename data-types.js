4937923ee024d4a74aaaaa3e1c63a4dc8ea708e3
Sean Biller <seanmbiller@gmail.com>
2019-09-30 21:10:53 -0400

    First Commit

---
+++ data-types.js
@@ -0,0 +1,23 @@
+var arraywithnumbersandstring = [1, '2', 3]
+var boolean = true || false
+var string = ("I am a string")
+var number = 9
+var date = new Date()
+var arrayofobjects = [ 
+    {attribute: 'Dave', anotherattribute: 'Tall'}
+    {attribute: 'Steve', anotherattribute: 'Tall'}
+    {attribute: 'Bob', anotherattribute: 'Short'}
+]
+
+
+const byheight = arrayofobjects.reduce((accumulator, arrayofobject) => {
+    const { attribute, anotherattribute } = arrayofobject
+    const
+}
+)
+
+console.log(arraywithnumbersandstring)
+console.log(boolean)
+console.log(string)
+console.log (arrayofobjects)
+console.log(number)
