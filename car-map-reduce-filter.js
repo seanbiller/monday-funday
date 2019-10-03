ab4eb10351a83f2f537c1c07b7276e474192c480
Sean Biller <seanmbiller@gmail.com>
2019-10-02 20:39:57 -0400

    filter

---
+++ car-map-reduce-filter.js
@@ -0,0 +1,15 @@
+let cars = [
+    { make: 'Toyota', model: 'Tundra', year: 2018 },
+    { make: 'Toyota', model: 'Prius', year: 1995 },
+    { make: 'VW', model: 'Jetta', year: 2018 },
+
+]
+
+let oldCars = cars.filter( (car) => {
+    return car.year === 2018
+})
+
+let sameModel = cars.map( (car) => {
+    return 
+})
+console.log (oldCars)
