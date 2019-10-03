3bbbdc9c2f33b445ca869cb565d5e956bb827c59
Sean Biller <seanmbiller@gmail.com>
2019-10-02 21:23:40 -0400

    more stuff

--- car-map-reduce-filter.js
+++ car-map-reduce-filter.js
@@ -1,6 +1,6 @@
 let cars = [
     { make: 'Toyota', model: 'Tundra', year: 2018 },
-    { make: 'Toyota', model: 'Prius', year: 1995 },
+    { make: 'Ford', model: 'Fusion', year: 1995 },
     { make: 'VW', model: 'Jetta', year: 2018 },
 
 ]
@@ -9,7 +9,16 @@ let oldCars = cars.filter( (car) => {
     return car.year === 2018
 })
 
-let sameModel = cars.map( (car) => {
-    return 
+// get an object that includes the existing data as well
+// as a single full string property named 'display'
+// for example '2012 ford fusion
+// '"${car.year} ${car.make} ${car.model}'
+displayData = cars.map((car) => {
+    let newProperty = car
+    newProperty.display = car.year + ' ' + car.make + ' ' + car.model
+    return newProperty
 })
-console.log (oldCars)
+
+
+
+console.log (displayData)
